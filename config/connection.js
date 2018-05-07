487d0d57770219761b1a35beab115c5004748773
Falanga <stefano.falanga94@gmail.com>
2018-05-07 16:27:36 -0400

    ..

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,9 @@
 
 var mysql = require("mysql");
+if (process.env.JAWSDB_URL) connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+else {
+
 
 var connection = mysql.createConnection({
   port: 3306,
@@ -8,7 +12,7 @@ var connection = mysql.createConnection({
   password: "password1",
   database: "burgers_db"
 });
-
+}
 
 connection.connect(function(err) {
   if (err) {

44f58bfe3331db2b8d4eb066449ecd46f581c30f
Falanga <stefano.falanga94@gmail.com>
2018-05-09 16:50:08 -0400

    ..

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-var PORT = process.env.PORT || 3080;
+var PORT = process.env.PORT || 8080;
 
 
 var port = 3000;
